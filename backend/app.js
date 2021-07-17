68fe7032d81f42b1eaaaed4c539e23ae60fd7f90
mehedirana <mdmehedirana@gmail.com>
2021-07-18 01:10:43 +0600

    first get req done

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const express = require('express')
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('yes mehedi get route done')
+})
+
+app.listen(3000,()=>{
+ console.log('server is running http://localhost:3000');   
+})
