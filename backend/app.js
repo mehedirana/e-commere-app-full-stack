e0099736c04f6121c0da0be3183cfd843973665f
mehediryans <mdmehedi.ryans@gmail.com>
2021-07-18 12:37:36 +0600

    get and post added for products

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,22 @@ const app = express();
 require("dotenv/config")
 const api = process.env.API_URL;
 
-app.get('/',(req,res)=>{
-    res.send('yes mehedi get route done')
+// middleware
+app.use(express.json());
+
+app.get(`${api}/products`,(req,res)=>{
+    const products = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(products)
+})
+app.post(`${api}/products`,(req,res)=>{
+
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct)
 })
 
 app.listen(3000,()=>{
