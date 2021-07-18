d8aab034fbcf086d008f9a06d32d01e329403a88
mehediryans <mdmehedi.ryans@gmail.com>
2021-07-18 12:23:07 +0600

    dot env added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
 
+require("dotenv/config")
+const api = process.env.API_URL;
+
 app.get('/',(req,res)=>{
     res.send('yes mehedi get route done')
 })
 
 app.listen(3000,()=>{
+    console.log("api url", api);
  console.log('server is running http://localhost:3000');   
 })
