f5be8a6a3849e4edcf1bb21be63cb6a057516463
mehediryans <mdmehedi.ryans@gmail.com>
2021-07-18 12:43:09 +0600

    middleware added

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 require("dotenv/config")
@@ -6,6 +7,7 @@ const api = process.env.API_URL;
 
 // middleware
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get(`${api}/products`,(req,res)=>{
     const products = {
