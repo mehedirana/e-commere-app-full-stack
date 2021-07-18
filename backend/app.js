2fdf63cee5db5ee8ef86b589782dd30760573d43
mehediryans <mdmehedi.ryans@gmail.com>
2021-07-18 14:45:34 +0600

    products get and post done

--- backend/app.js
+++ backend/app.js
@@ -10,19 +10,39 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`,(req,res)=>{
-    const products = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
     }
-    res.send(products)
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+app.get(`${api}/products`, async (req,res)=>{
+    const productList = await Product.find();
+    res.send(productList)
 })
 app.post(`${api}/products`,(req,res)=>{
 
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct)
+    const product = new Product({
+       name: req.body.name,
+       image: req.body.image,
+       countInStock: req.body.countInStock,
+    })
+    // const newProduct = req.body;
+    // console.log(newProduct);
+    product.save().then((createdProduct=>{
+        res.status(201).json(createdProduct)
+    })).catch((err)=> {
+        res.status(500).json({
+            error:err,
+            success: false
+        })
+    })
+    // res.send(newProduct)
 })
 
 mongoose.connect(process.env.CONNECTION_STRING,{ 
@@ -31,7 +51,7 @@ mongoose.connect(process.env.CONNECTION_STRING,{
     useFindAndModify: false,
     useUnifiedTopology: true,
     useCreateIndex: true  })
-    
+
 .then(()=> console.log('db connection ready'))
 .catch((err)=> console.log(' error msg ', err))
 app.listen(3000,()=>{
