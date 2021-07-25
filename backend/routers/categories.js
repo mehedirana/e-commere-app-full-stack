8956a480c75167c346a37064e8ab54e6ad0fbfef
mehedirana <mdmehedirana@gmail.com>
2021-07-25 16:52:51 +0600

    category details added

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -1,5 +1,6 @@
 const Category = require('../models/category');
 const express = require('express');
+const category = require('../models/category');
 const router = express.Router();
 
 router.get('/', async (req,res)=>{
@@ -13,6 +14,28 @@ router.get('/', async (req,res)=>{
 
 })
 
+
+router.get('/:id', async (req, res)=>{
+    // const category = await Category.findById(req.params.id);
+    
+    // if(!category){
+    //     return res.status(500).send({success: false,messgae: 'The category of the given ID was not found!'})
+    // }
+   
+    // res.status(200).send({success: true, category})
+
+    Category.findById(req.params.id).then(category =>{
+        if(category){
+            return res.status(200).json({success: true, category})
+        }
+        else{
+            return res.status(404).json({success: false, message: 'The category of the given ID was not found!'})
+        }
+    }).catch(err =>{
+        return res.status(400).json({success: false, error: err})
+    })
+})
+
 router.post('/', async (req, res)=>{
     let category = new Category({
         name: req.body.name,
@@ -21,9 +44,9 @@ router.post('/', async (req, res)=>{
     })
      
     category = await category.save();
-    const msg ="Category cannot be created"
-    if(!category) return res.status(404).send({msg, success: false})
-     res.send({category, success: true})
+    
+    if(!category) return res.status(404).send({message:'Category cannot be created', success: false})
+     res.status(200).send({category, success: true})
 
 })
 
