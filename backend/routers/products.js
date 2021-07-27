4e0446acb608558f90f1da3500293dee96eab518
mehedirana <mdmehedirana@gmail.com>
2021-07-27 23:07:03 +0600

    catery and products api

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -1,10 +1,12 @@
 const Product = require('../models/product')
 const express = require('express');
+// const category = require('../models/category');
+const Category = require('../models/category');
 const router = express.Router()
 
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find();
+    const productList = await Product.find().populate('category');
 
     if (!productList) {
         res.status(500).json({ success: false })
@@ -27,31 +29,42 @@ router.get('/:id', async (req, res) => {
     }
 
 })
-router.post(`/`, (req, res) => {
+router.post(`/`, async(req, res) => {
+
+    const category = await Category.findById(req.body.category)
+    if(!category) return res.status(400).send('Invalid Category')
 
     const product = new Product({
         name: req.body.name,
         description: req.body.description,
         richDescription: req.body.richDescription,
         image: req.body.image,
-        images: req.body.images,
+        // images: req.body.images,
         brand: req.body.brand,
         price: req.body.price,
         category: req.body.category,
         countInStock: req.body.countInStock,
         rating: req.body.rating,
         numReviews: req.body.numReviews,
-        isFeatured: req.body.isFeatured,
-        dateCreated: req.body.dateCreated
+        // isFeatured: req.body.isFeatured,
+        // dateCreated: req.body.dateCreated
     })
-    product.save().then((createdProduct => {
-        res.status(201).json({ createdProduct, success: true })
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+
+    product = await product.save();
+
+    if(!product){
+        return res.status(500).json({error: 'product can be created',success: false})
+    }
+
+    return res.send(product)
+    // product.save().then((createdProduct => {
+    //     res.status(201).json({ createdProduct, success: true })
+    // })).catch((err) => {
+    //     res.status(500).json({
+    //         error: err,
+    //         success: false
+    //     })
+    // })
 })
 
 module.exports = router;
