79eba8243b447e35950a925f1ee4dc7374bd63da
mehedirana <mdmehedirana@gmail.com>
2021-07-31 19:28:25 +0600

    product delete added

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -98,4 +98,18 @@ router.put('/:id', async (req, res) => {
 
 })
 
+// delete a product 
+
+router.delete('/:id', async (req, res)=>{
+    try {
+        const product = await Product.findByIdAndRemove(req.params.id)
+        if(product) return res.status(200).json({success: true, message: 'The product is deleted!'});
+        else return  res.status(404).json({success: false, message: 'Product not found!'})
+    } catch (error) {
+        res.status(400).json({success: false, error: error})
+    }
+    
+
+})
+
 module.exports = router;
