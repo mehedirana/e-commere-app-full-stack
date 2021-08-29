1086457b4a6d7ddc090e03f4c0597b83e2a98264
mehediryans <mdmehedi.ryans@gmail.com>
2021-08-29 12:14:19 +0600

    order post route created

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -11,4 +11,25 @@ router.get('/', async (req,res)=>{
     res.send(orderList)
 })
 
+router.post('/', async (req, res)=>{
+ let order = new Order({
+    orderItems: req.body.orderItems, 
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+ })
+
+ order = await order.save();
+ if(!order) 
+    return res.status(400).send('the order can not be created');
+
+  res.status(200).json({success: true, order})
+})
+
 module.exports = router;
